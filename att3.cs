dc76d983a4722cbc40572d9d8f6e87554a84b68b
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-06-18 17:35:14 -0300

    Update att3.cs

--- att3.cs
+++ att3.cs
@@ -9,92 +9,88 @@ namespace att3
     class Program
     {
         static void Main(string[] args)
-        {
-
-            Console.WriteLine("De a cor do peão:");
+          Console.WriteLine("Digite a cor do peão:");
             string cor = Console.ReadLine();
-            Console.WriteLine("De o numero para identificar o peão o peão:");
-             int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o número para identificar o peão:");
+            int id = int.Parse(Console.ReadLine());
 
             Peao p = new Peao(cor, id);
 
-            string resp = "S";
+            string resp = "A";
+            Random r = new Random();
 
-            while (resp == "S")
+            while (resp != "S")
             {
+                Console.WriteLine();
                 Console.WriteLine($"Peão:{p.Identificador} Cor:{p.Cor} Posição:{p.Posicao}");
 
-                Console.WriteLine("Mover = M , s");
-                resp = Console.ReadLine();
-                if (resp == "S" )
+                int m = r.Next(1,10); 
+
+                Console.WriteLine($"O peão irá mover {m} casas");
+
+                Console.WriteLine("Mover = M , Retornar = R , Sair = S");
+                resp = Console.ReadLine().ToUpper();
+
+                switch (resp)
                 {
-
-                    Random r = new Random();
-
-                    int m = r.Next(10);
-
-                    p.Mover(m);
+                    case "M":
+                        p.Mover(m);
+                        break;
+                    case "R":
+                        p.RetornarCasa(m);
+                        break;
+                    case "S":
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
                 }
-           
-
-
             }
 
-
-
-
-
+            Console.WriteLine($"Peão:{p.Identificador} Cor:{p.Cor} Posição:{p.Posicao}");
+            Console.ReadLine();
         }
-           
 
+        class Peao
+        {
+            private string cor;
+            private int identificador;
+            private int posicao;
 
+            public Peao(string cor, int identificador)
+            {
+                this.cor = cor;
+                this.identificador = identificador;
+                this.posicao = 0;
+            }
+
+            public string Cor
+            {
+                get { return cor; }
+                set { cor = value; }
+            }
+
+            public int Identificador
+            {
+                get { return identificador; }
+                set { identificador = value; }
+            }
+
+            public int Posicao
+            {
+                get { return posicao; }
+                set { posicao = value; }
+            }
+
+            public void Mover(int m)
+            {
+                posicao += m;
+            }
+
+            public void RetornarCasa(int m)
+            {
+                posicao -= m;
+            }
         }
     }
-
-
-    class Peao
-    {
-        private string cor;
-        private int identificador;
-        private int posicao;
-
-        public Peao(string cor, int indentificador)
-        {
-            this.cor = cor;
-            this.identificador = indentificador;
-            this.posicao = 0;
-        }
-
-        public string Cor
-        {
-            get { return cor; }
-            set { cor = value; }
-        }
-
-        public  int Identificador
-        {
-            get { return identificador; }
-            set { identificador = value; }
-        }
-
-        public int Posicao
-        {
-            get { return posicao; }
-            set { posicao = value; }
-        }
-
-
-        public void Mover(int m)
-        {
-            this.Posicao += m;
-        }
-
-        public void RetornarCasa(int m)
-        {
-            this.Posicao -= m;
-        }
-
-
-    }
-   
-
+}
